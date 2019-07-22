1e704c2b612d99f578b1d0f6fe7ac8281a566157
Promila Tamanggit config --global user.email promilatam@gmail.com <promilatam@gmail.com>
2019-07-22 15:54:50 -0700

    checkpoint 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from "react-router-dom";
+import { Redirect } from 'react-router';
 import Aside from './Aside.js';
 import './../CSS/App.css';
 
 
+
 export default class Login extends Component {
- 
+  constructor(props){
+    super(props);
+    this.state={isLogin: false};
+    this.loginClick = this.loginClick.bind(this);
+  }
+  loginClick(event){
+    event.preventDefault();
+    console.log(event.target["username"].value);
+    this.props.history.push('/search');
+    console.log(event.target["username"].value);
+    
+  }
   render () {
     return (
       <Fragment>
@@ -19,12 +32,12 @@ export default class Login extends Component {
         <main>
           <h1>Welcome: Operation Breach</h1>
           <p>Description of our services</p>
-          <form>
+          <form onSubmit={this.loginClick}>
             <label for="username">Username: </label>
             <input name="username" type="text" placeholder="Username Here"></input>
             <label for="password">Password</label>
             <input name="password" type="password" placeholder="Password"></input>
-            {/* <input type="submit" value="submit">Log In</input> */}
+            <button type="submit" value="submit">Log In</button>
           </form>
           <Aside />
         </main>
