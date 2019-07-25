3444472894cf6d48d84957a6c0fb45e4d932f150
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-25 09:38:29 -0700

    deployment for heroku.  built production files, deleted homepage entry in package.json.

---
+++ client/build/precache-manifest.d150dde840e618a0a6d32bedbe90b05d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8c154b7ad850e90724b128f689e26",
+    "url": "/deep-diving-data-weasels/operation-breach/index.html"
+  },
+  {
+    "revision": "c036a838e3713141a2f5",
+    "url": "/deep-diving-data-weasels/operation-breach/static/css/main.3c418a43.chunk.css"
+  },
+  {
+    "revision": "d3a42bbdc4e910cde60e",
+    "url": "/deep-diving-data-weasels/operation-breach/static/js/2.b0f3b7c3.chunk.js"
+  },
+  {
+    "revision": "c036a838e3713141a2f5",
+    "url": "/deep-diving-data-weasels/operation-breach/static/js/main.b8690ecf.chunk.js"
+  },
+  {
+    "revision": "889ea9f5b8304958d1da",
+    "url": "/deep-diving-data-weasels/operation-breach/static/js/runtime~main.2e63e4b5.js"
+  }
+]);
