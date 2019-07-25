126fc6295a8fea6363a782d0ea7c442d12084bd5
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-25 10:59:46 -0700

    changed routing to search page for heroku in search.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,11 @@
+//libraries
 import React, { Component, Fragment } from 'react';
 import { Route } from "react-router-dom";
-
+//pages
 import AboutUs from './AboutUs.js';
 import Login from './Login.js';
 import Search from './Search.js';
 import Results from './Results.js';
-
 
 export default class LandingPage extends Component {
   constructor(props){
@@ -13,7 +13,7 @@ export default class LandingPage extends Component {
    this.setData = this.setData.bind(this); 
    this.state = {};
   }
-
+  //state
   setData(data) {
     this.setState(data);
   }
@@ -24,7 +24,7 @@ export default class LandingPage extends Component {
       <Fragment>
         <Route exact path="/" component={Login} />
         <Route exact path="/search" component={() => < Search callback={this.setData} />} />
-        <Route path='/results' component={() => < Results apiPwnd={this.state.pwndResult|| []} apiSocial={this.state.socialResult || {'posts': []}}/>} />
+        {/* <Route path='/results' component={() => < Results apiPwnd={this.state.pwndResult|| []} apiSocial={this.state.socialResult || {'posts': []}}/>} /> */}
         <Route exact path="/aboutus" component={AboutUs} />
       </Fragment>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -12,34 +12,28 @@ let dataSocial;
 
 // Search class
 export default  withRouter( class Search extends Component {
- 
   constructor(props){
     super(props);
     this.searchPwnd = this.searchPwnd.bind(this);
     this.searchSocial = this.searchSocial.bind(this);
     this.submitHandle = this.submitHandle.bind(this);
-    // this.searchPassword = this.searchPassword.bind(this);
   } // constructor end
 
   async searchPwnd(email) {
-
    //for local testing
     // const backEndURL = "http://localhost:3000/apiPwnd";
     const url = 'https://operation-breach.herokuapp.com/apiPwnd';
-
     superagent.get(url)
       .query({data: email})
       .then(res => {
         dataPwnd = res.body;
       console.log(dataPwnd);
-      this.props.callback({pwndResult: dataPwnd});
-      
+      // this.props.callback({pwndResult: dataPwnd}); 
+      this.setState({pwndResult: dataPwnd});   
     });
 
   } // searchPwnd end
-
   async searchSocial(email) {
-
    //for local testing
     // const backEndURL = "http://localhost:3000/apiSocial";
     const url = 'https://operation-breach.herokuapp.com/apiSocial';    
@@ -48,11 +42,11 @@ export default  withRouter( class Search extends Component {
       .then(res => {
         dataSocial = res.body;
       console.log(dataSocial);
-      this.props.callback({socialResult: dataSocial});
+      // this.props.callback({socialResult: dataSocial});
+      this.setState({socialResult: dataSocial});
       // this line must be call on the last API route 
-      this.props.history.push("/results");
+      this.props.history.push("/search/results");
     });
-
   } // searchSocial end
 
   async submitHandle(event){
@@ -60,12 +54,7 @@ export default  withRouter( class Search extends Component {
     let email = event.target["email"].value;
     await this.searchPwnd(email);
     await this.searchSocial(email);
-    
   }
-
-  // searchPassword(event) {
-
-  // }
  
   render () {
     return (
@@ -80,15 +69,10 @@ export default  withRouter( class Search extends Component {
             <input name="email" type="text" placeholder="E-mail Address "></input>
             <button type="submit" value="submit">Search</button>
           </form>
-          {/* <form>
-            <label for="Password">Password: </label>
-            <input name="Password" type="text" placeholder="Password"></input>
-            <input type="submit" value="submit">Search</input>
-          </form> */}
           </div>
           <h3>placeholder</h3>
           <p>we don't sell any kind of Password</p>
-
+          <Route path='/search/results' component={() => < Results apiPwnd={this.state.pwndResult|| []} apiSocial={this.state.socialResult || {'posts': []}}/>} />
           <Aside />
         </main>
         <Footer />
