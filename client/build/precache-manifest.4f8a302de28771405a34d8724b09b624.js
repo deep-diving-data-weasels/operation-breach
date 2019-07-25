3b9c1a87ea8c692fcfe6bd0a61ea20b25af5dfbe
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-25 11:07:44 -0700

    hotfix

---
+++ client/build/precache-manifest.4f8a302de28771405a34d8724b09b624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3523905e4dca0fa65c7935733396ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5336573d9bf871d523d",
+    "url": "/static/css/main.3c418a43.chunk.css"
+  },
+  {
+    "revision": "55c4c71ff069b522542e",
+    "url": "/static/js/2.501ca953.chunk.js"
+  },
+  {
+    "revision": "d5336573d9bf871d523d",
+    "url": "/static/js/main.69618ea9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
